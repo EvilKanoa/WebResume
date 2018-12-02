95a3821e6773a0b7cb7606e6fd80881ca898036b
EvilKanoa <kanoa@kanoa.ca>
2018-12-02 01:59:22 -0500

    Added default resume

--- editor/src/reducer.js
+++ editor/src/reducer.js
@@ -6,8 +6,10 @@ import {renderToStaticMarkup} from 'react-dom/server'
 import storage from 'core/storage';
 import Resume from 'components/resume/Resume';
 
+const defaultResume = `{"data":{"name":"The Doctor","title":"Time Lord","contact":[{"icon":"fas fa-at","link":"mailto:doctor@tardis.time","label":"doctor@tardis.time"},{"icon":"fas fa-phone","link":"tel:+1234567890","label":"(123) 456-7890"}],"details":[{"type":"summary","label":"Profile","summary":"Looking to run from all sorts of trouble."},{"type":"skills","skillsets":[{"label":"Languages","skills":"English, French, Gallifreyan"}]},{"type":"details","label":"Interests","items":[{"title":"Traveling","description":"All over the universe"}]}],"events":[{"label":"Experience","icon":"fas fa-briefcase","items":[{"title":"Space Pilot","subtitle":"Milky way","date":"May 2048 BC .. Dec 6000 AC","description":"Went for a wild ride.","details":["Lots of different places"]}]}]}}`;
+
 const initialState = {
-    resume: storage.get('resume') || {},
+    resume: storage.get('resume') || JSON.parse(defaultResume),
     render: '',
     printMode: false
 };
